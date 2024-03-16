c90133f0b275555f233fa2e85e0f28c23bde91fc
dzakiyaalfyatunnufus02 <dzakiyaalfyatunnufus@gmail.com>
2024-03-16 12:19:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./component/Home"
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <BrowserRouter>
+   <main>
+    <Switch>
+      <Route path="/" component={Home} exact />
+    </Switch>
+    </main></BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,262 @@
+import React from "react";
+import "../css/Home.css";
+import SMKPK from "../aset/SMK PK.png";
+import Data from "../aset/pngtree-office-cartoon-data-image_1194691-removebg-preview.png";
+import Pembayaran from "../aset/bayar.png"
+import Tabungan from "../aset/pembayran.png"
+import Keungan from "../aset/uang.png"
+import Inventaris from "../aset/inventaris.png" 
+import Ziswaf from "../aset/Ziswaf.png"
+import PPDB from "../aset/siswa_sma-removebg-preview.png"
+import Komunikasi from "../aset/Komunikasi.png"
+import APBS from "../aset/apbs.png"
+import Kinerja from "../aset/Kinerja (2).png"
+import Monitoring from "../aset/Monitoring.png"
+import Uang from "../aset/uang-saku.png";
+import Logo from "../aset/logo.png"
+const Home = () => {
+  return (
+    <div class="context" >
+      <center className="Bg">
+        <table
+          align="center"
+          className="table-flex"
+          border="0"
+        >
+          <tr >
+            <td colspan="2">
+              <img
+                src="https://aksesbersama.my.id/assets/img/logo-sikopin-biru.png"
+                className="height-img"
+              />
+            </td>
+            <td colspan="2" align="right">
+              <img src={Logo} className="height-img" />
+            </td>
+          </tr>
+
+          <tr align="center" className="flex-gap-tr"  >
+            <td className="width: 20%  gap-flex" >
+              <div 
+                class="box"
+                onclick="window.location='apps/index.php?menu=PENDATAAN'"
+              >
+                <div class="our-services backups">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Data}
+                      className="border-radius: 20px width-img-70"
+                    />{" "}
+                  </div>
+                  <h3 style={{textAlign:"center"}}>PENDATAAN</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex" >
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=PEMBAYARAN'"
+              >
+                <div class="our-services backups">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Pembayaran}
+                      className="border-radius: 20px width-img-72"
+                    //   style={{fontSize: "1.5rem"}}
+                    />{" "}
+                  </div>
+                  <h3>PEMBAYARAN</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=TABUNGAN'"
+              >
+                <div class="our-services backups">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Tabungan}
+                      className="border-radius: 20px width-img-83"
+                    />{" "}
+                  </div>
+                  <h3>TABUNGAN</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=KEUANGAN'"
+              >
+                <div class="our-services backups">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Keungan}
+                      className="border-radius: 20px width-img-60"
+                    />{" "}
+                  </div>
+                  <h3>KEUANGAN</h3>
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr align="center">
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=INVENTARIS'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Inventaris}
+                      className="border-radius: 20px width-img-120"
+                    />{" "}
+                  </div>
+                  <h3>INVENTARIS</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=ZISWAF'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Ziswaf}
+                      className="border-radius: 20px width-img-100"
+                    />{" "}
+                  </div>
+                  <h3>ZISWAF</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=PPD'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={PPDB}
+                      className="border-radius: 20px width-img-120"
+                    />{" "}
+                  </div>
+                  <h3>PPD</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=KOMUNIKASI'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Komunikasi}
+                      className="border-radius: 20px width-img-100"
+                    />{" "}
+                  </div>
+                  <h3>KOMUNIKASI</h3>
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr align="center">
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=APBS'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={APBS}
+                      width="120px"
+                      className="border-radius: 20px width-img-120"
+                    />{" "}
+                  </div>
+                  <h3>APBS</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=KINERJA'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Kinerja}
+                      className="border-radius: 20px width-img-80"
+                    />{" "}
+                  </div>
+                  <h3>KINERJA</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=KONSOLIDASIAN'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Monitoring}
+                      className="border-radius: 20px width-img-80"
+                    />{" "}
+                  </div>
+                  <h3>MONITORING</h3>
+                </div>
+              </div>
+            </td>
+            <td className="width: 20% gap-flex">
+              <div
+                class="box"
+                onclick="window.location='apps/index.php?menu=UANGSAKU'"
+              >
+                <div class="our-services settings">
+                  <div class="icon">
+                    {" "}
+                    <img
+                      src={Uang}
+                      className="border-radius: 20px width-img-80"
+                    />{" "}
+                  </div>
+                  <h3>UANG SAKU</h3>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="4" align="center" style={{paddingTop:"30px", paddingBottom:"30px"}}>
+              <p style={{color:"black"}}>Copyright &copy; Sikopin 2021</p>
+            </td>
+          </tr>
+        </table>
+      </center>
+    </div>
+  );
+};
+export default Home;
