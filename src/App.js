e0ed4075bf060a204dbcac698dac023eb6900b03
projectbinusa <project@binusasmg.sch.id>
2024-06-04 10:57:24 +0700

    update fe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from "./component/Home"
-import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import Login from './component/Login';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./component/Home";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Login from "./component/Login";
 
 function App() {
   return (
     <div className="App">
-   <BrowserRouter>
-   <main>
-    <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/login" component={Login} exact />
-    </Switch>
-    </main></BrowserRouter>
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/login" component={Login} exact />
+          </Switch>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -16,10 +16,11 @@ import Kasir from "../aset/kasir.png";
 import Gelembung from "../aset/gelembung.png";
 import Logo from "../aset/LOGO_Katalog.png";
 import PT from "../aset/pt-dinartech.png";
+import Lab from "../aset/programmer.png";
+
 const Home = () => {
-  
   useEffect(() => {
-    AOS.init(); 
+    AOS.init();
   }, []);
   return (
     <div className="Bg">
@@ -43,11 +44,7 @@ const Home = () => {
               </tr>
 
               <tr align="center" className="flex-gap-tr">
-                <td
-                  className="width: 20%  gap-flex"
-                  
-                  
-                >
+                <td className="width: 20%  gap-flex">
                   <div className="box wow fadeInLeft slow">
                     <div className="our-services backups">
                       <div className="icon">
@@ -67,7 +64,7 @@ const Home = () => {
                     <h2></h2>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex" >
+                <td className="width: 20% gap-flex">
                   <div className="">
                     <div className="">
                       <div className="box">
@@ -90,8 +87,8 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex" >
-                  <div className="box" >
+                <td className="width: 20% gap-flex">
+                  <div className="box">
                     <div className="our-services backups">
                       <div className="icon">
                         {" "}
@@ -104,8 +101,7 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex " 
-                >
+                <td className="width: 20% gap-flex ">
                   <div className="box">
                     <div className="our-services backups">
                       <div className="icon">
@@ -121,7 +117,7 @@ const Home = () => {
                 </td>
               </tr>
 
-              <tr align="center" className="flex-gap-tr" >
+              <tr align="center" className="flex-gap-tr">
                 <td className="width: 20% gap-flex">
                   <div className="box">
                     <div className="our-services settings">
@@ -136,8 +132,7 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex" 
-                >
+                <td className="width: 20% gap-flex">
                   <div className="box">
                     <div className="our-services settings">
                       <div className="icon">
@@ -151,8 +146,7 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex" 
-                >
+                <td className="width: 20% gap-flex">
                   <div className="box">
                     <div className="our-services settings">
                       <div className="icon">
@@ -166,7 +160,7 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
-                <td className="width: 20% gap-flex" >
+                <td className="width: 20% gap-flex">
                   <div className="box">
                     <div className="our-services settings">
                       <div className="icon">
@@ -239,6 +233,66 @@ const Home = () => {
                     </div>
                   </div>
                 </td>
+              </tr>
+
+              {/* Row 4 */}
+              <tr align="center" className="flex-gap-tr">
+                <td className="width: 20% gap-flex" colSpan="1">
+                  <div className="box">
+                    <div className="our-services settings">
+                      <div className="icon">
+                        {" "}
+                        <img
+                          src={Lab}
+                          className="border-radius: 20px width-img-122 apbs"
+                        />{" "}
+                      </div>
+                      <h3>Lab Bahasa</h3>
+                    </div>
+                  </div>
+                </td>
+                {/* <td className="width: 20% gap-flex">
+                  <div className="box">
+                    <div className="our-services settings">
+                      <div className="icon">
+                        {" "}
+                        <img
+                          src={Invit}
+                          className="border-radius: 20px width-img-88 kinerja"
+                        />{" "}
+                      </div>
+                      <h3>E-Invitation</h3>
+                    </div>
+                  </div>
+                </td>
+                <td className="width: 20% gap-flex">
+                  <div className="box">
+                    <div className="our-services settings">
+                      <div className="icon">
+                        {" "}
+                        <img
+                          src={Monitoring}
+                          className="border-radius: 20px width-img-80 monitoring"
+                        />{" "}
+                      </div>
+                      <h3>Data Center</h3>
+                    </div>
+                  </div>
+                </td>
+                <td className="width: 20% gap-flex">
+                  <div className="box">
+                    <div className="our-services settings">
+                      <div className="icon">
+                        {" "}
+                        <img
+                          src={Kasir}
+                          className="border-radius: 20px width-img-80 monitoring"
+                        />{" "}
+                      </div>
+                      <h3>Kasir</h3>
+                    </div>
+                  </div>
+                </td> */}
               </tr>
               <tr>
                 <td
